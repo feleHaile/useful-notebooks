6f55fbe5f05e2ae1c28fb80e1311a0a40b5b8550
pankajkarman <34066356+pankajkarman@users.noreply.github.com>
2019-02-18 17:59:52 +0530

    Add files via upload

--- bias_correction.py
+++ bias_correction.py
@@ -1,24 +1,17 @@
-import numpy as np
 from statsmodels.distributions.empirical_distribution import ECDF
-
-# data required reference to correct with respect to, control data for the overlapping period
-
-# modified quantile correction based on Bai et. al. (2016) 
-def modified_quantile_correction(ref, ctr, data):
+def quantile_correction(ref, ctr, data, modified = True):
     cdf = ECDF(ctr)
     p = cdf(data)
         
-    cor = np.subtract(*np.nanquantile([ref, ctr], p))
-    mid = np.subtract(*np.nanquantile([ref, ctr], 0.5))
+    cor = np.subtract(*[np.nanquantile(x, p) for x in [ref, ctr]])
+    mid = np.subtract(*[np.nanquantile(x, 0.5) for x in [ref, ctr]])
+    g = np.true_divide(*[np.nanquantile(x, 0.5) for x in [ref, ctr]])
     
     iqr_ref = np.subtract(*np.nanquantile(ref, [0.75, 0.25]))
-    iqr_ctr = np.subtract(*np.nanquantile(ctr, [0.75, 0.25]))
-    
-    g = np.true_divide(*np.nanquantile([ref, ctr], 0.5))
+    iqr_ctr = np.subtract(*np.nanquantile(ctr, [0.75, 0.25])) 
     f = np.true_divide(iqr_ref, iqr_ctr)
-    
-    correction = g*mid + f*(cor - mid)
-    return data + correction
-    
-
-
+    corr = g*mid + f*(cor - mid)
+    if modified:
+        return data + corr
+    else:   
+        return data + cor
